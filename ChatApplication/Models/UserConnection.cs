e030c3908cf921064f4fc11310b055dfbdcf650d
vibha2 <juhisahu2203@gmail.com>
2024-08-06 18:28:11 +0530

    backend

---
+++ ChatApplication/Models/UserConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApplication.Models
+{
+    public class UserConnection
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string ChatRoom { get; set; } = string.Empty;
+
+    }
+}
